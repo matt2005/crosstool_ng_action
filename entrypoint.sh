dd53db0f13b4ddcfb78d1e1898335cc1608256a0
suda-morris <362953310@qq.com>
2019-11-03 06:14:26 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,12 @@ set -e
 
 mkdir ${INPUT_WORK_DIR}
 cd ${INPUT_WORK_DIR}
-cp /arm9-linux-gnueabi.config .config
-ct-ng build --silent
+if [ "${INPUT_ARCH}" == "arm9" ]; then
+    cp /arm9-linux-gnueabi.config .config
+else
+    echo "${INPUT_ARCH} is not supported for now!"
+fi
+ct-ng build
+cd x-tools/bin
+./arm-unknown-linux-gnueabi-gcc -v
 exec "$@"
